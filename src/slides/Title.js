f176d15539f7e390d7a2709a85244eebfbca8949
Nicholas Scheurich <nick@scheurich.me>
2018-02-12 18:03:56 -0600

    Make title slide conform to Activate specs

--- src/slides/Title.js
+++ src/slides/Title.js
@@ -1,15 +1,32 @@
 import React from "react";
-import { Slide, Heading, Text, Typeface } from "spectacle";
+import { Slide, Image } from "spectacle";
+
+import titleImage from "../images/title-slide.jpg";
+
+const fullScreenStyle = {
+  alignItems: "center",
+  flexDirection: "column",
+  top: "calc(-50vh + 50%)",
+  left: "calc(-50vw + 50%)",
+  position: "absolute",
+  width: "100vw",
+  height: "100vh"
+};
+
+const imageStyle = {
+  margin: 0,
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)"
+};
 
 export default () => {
   return (
-    <Slide>
-      <Heading lineHeight={1.2} textSize="60">
-        Intro to Functional Programming with Elixir
-      </Heading>
-      <Text textSize="30" textColor="rgba(0, 0, 0, 0.6)" margin={21}>
-        Logan Leger
-      </Text>
+    <Slide bgColor="#0a3d5e">
+      <div style={fullScreenStyle}>
+        <Image src={titleImage} style={imageStyle} />
+      </div>
     </Slide>
   );
 };
