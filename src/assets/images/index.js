4f134fe40dee5c0d8c86467750d815fb2a97e43d
Nicholas Scheurich <nick@scheurich.me>
2018-02-21 01:47:34 -0600

    Put images in assets dir

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+import titleSlide from "./title-slide.jpg";
+import erlangLogo from "./erlang-logo.svg";
+import elixirLogo from "./elixir-logo.png";
+import exampleFunction from "./function.svg";
+
+export { titleSlide, erlangLogo, elixirLogo, exampleFunction };
